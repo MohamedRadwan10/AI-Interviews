3da93ab6fbdaf2fd7a2bc6b311d687cf8621a3d8
MohamedRadwan10 <Mohamedalaa0601@gmail.com>
2026-01-26 22:33:25 +0200

    test commit

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div className='bg-blue-50' >Home</div>
-  )
-}
+    <div className="bg-blue-50">
+      <h1 className="bg-slate-950">Home</h1>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
